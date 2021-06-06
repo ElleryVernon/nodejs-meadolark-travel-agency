df8b373c8a49c95e5d47c6796d56ffc7b939bb37
ElleryVernon <tmpks3@gmail.com>
2021-06-07 03:45:56 +0900

    add QA part

--- meadowlark.js
+++ meadowlark.js
@@ -1,7 +1,19 @@
+const handlers = require("./lib/handlers")
 const express = require("express")
 const expressHandlebars = require("express-handlebars")
 const fortune = require("./lib/fourtune")
 const app = express()
+
+app.get("/", handlers.home)
+
+app.get("/about", handlers.about)
+
+// custom 404 page
+app.use(handlers.notFound)
+
+// custom 500 page
+app.use(handlers.severError)
+
 // 핸들바 뷰 설정
 app.engine(
   "handlebars",
@@ -28,13 +40,19 @@ app.use((req, res) => {
 })
 
 // custom 500 page.
+/* eslint-disable no-unused-vars */
 app.use((err, req, res, next) => {
   console.error(err.message)
   res.status(500)
   res.render("500")
 })
+/* eslint-disable no-unused-vars */
 
-app.listen(port, () => {
-  ;`Express started on http://localhost: ${port}` +
-    `press Ctrl + C to terminate`
-})
+if (require.main === module) {
+  app.listen(port, () => {
+    `Express started on http://localhost: ${port}` +
+      `press Ctrl + C to terminate.`
+  })
+} else {
+  module.exports = app
+}
